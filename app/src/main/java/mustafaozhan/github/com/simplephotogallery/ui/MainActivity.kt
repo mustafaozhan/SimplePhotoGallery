0c928340975689c649a4ba9efc1c60d80b570c73
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-12-21 18:52:49 +0300

    Main Activity Listeners implemented

--- app/src/main/java/mustafaozhan/github/com/simplephotogallery/ui/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/simplephotogallery/ui/MainActivity.kt
@@ -2,9 +2,11 @@ package mustafaozhan.github.com.simplephotogallery.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.view.Gravity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import mustafaozhan.github.com.simplephotogallery.R
-import mustafaozhan.github.com.simplephotogallery.model.Albums
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,19 +25,47 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setHomeAsUpIndicator(resources.getDrawable(R.drawable.ic_launcher_foreground))
 
-        setupNavigationView()
 
         val extra = intent.extras
         if (extra != null) {
             val extraData = extra.get("img_url_data") as ArrayList<*>
-            selectFragment(extraData)
+//            selectFragment(extraData)
         }
 
-        drawerLayoutListenner()
+        setListeners()
 
-        supportActionBar!!.setTitle("Folders")
+        supportActionBar!!.title = "Folders"
 
 
+    }
+
+    private fun setListeners() {
+
+        mNavigation.setNavigationItemSelectedListener { item ->
+            mDrawerLayout.closeDrawer(Gravity.START)
+            when (item.itemId) {
+
+            }
+            false
+        }
+
+
+        mDrawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerStateChanged(newState: Int) {
+            }
+
+            override fun onDrawerSlide(drawerView: View?, slideOffset: Float) {
+            }
+
+            override fun onDrawerClosed(drawerView: View?) {
+                supportActionBar!!.setHomeAsUpIndicator(resources.getDrawable(R.drawable.ic_launcher_foreground))
+            }
+
+            override fun onDrawerOpened(drawerView: View?) {
+                supportActionBar!!.setHomeAsUpIndicator(resources.getDrawable(R.drawable.abc_ic_ab_back_material))
+            }
+
+        })
     }
 
 
