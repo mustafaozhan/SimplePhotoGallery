685a829ee00f5f6e925b86b29777958a0aba1457
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-12-22 12:57:33 +0300

    Album Activity Added

---
+++ app/src/main/java/mustafaozhan/github/com/simplephotogallery/ui/activities/AlbumActivity.kt
@@ -0,0 +1,93 @@
+package mustafaozhan.github.com.simplephotogallery.ui.activities
+
+import android.app.Activity
+import android.database.Cursor
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.AbsListView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_album.*
+import mustafaozhan.github.com.simplephotogallery.R
+
+class AlbumActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album)
+
+        setSupportActionBar(my_album_toolbar)
+        // Enable the Up button
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val folderName = intent.getStringExtra("folder_name")
+        supportActionBar!!.setTitle("" + folderName)
+        val isVideo = intent.getBooleanExtra("isVideo", false)
+        init_ui_views(folderName, isVideo)
+
+    }
+    var adapter: SingleAlbumAdapter? = null
+
+    private fun init_ui_views(folderName: String?, isVideo: Boolean?) {
+
+        val options = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE).override(160, 160).skipMemoryCache(true).error(R.drawable.ic_image_unavailable)
+        val glide = Glide.with(this)
+        val builder = glide.asBitmap()
+
+        rvAlbumSelected.layoutManager = GridLayoutManager(this, 2)
+        rvAlbumSelected?.setHasFixedSize(true)
+        adapter = SingleAlbumAdapter(getAllShownImagesPath(this, folderName, isVideo), this, options, builder, glide, this)
+        rvAlbumSelected?.adapter = adapter
+
+        rvAlbumSelected?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                when (newState) {
+                    RecyclerView.SCROLL_STATE_IDLE -> glide.resumeRequests()
+                    AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL, AbsListView.OnScrollListener.SCROLL_STATE_FLING -> glide.pauseRequests()
+                }
+            }
+        }
+        )
+    }
+
+// Read all images path from specified directory.
+
+    private fun getAllShownImagesPath(activity: Activity, folderName: String?, isVideo: Boolean?): MutableList<String> {
+
+        val uri: Uri
+        val cursorBucket: Cursor
+        val column_index_data: Int
+        val listOfAllImages = ArrayList<String>()
+        var absolutePathOfImage: String? = null
+
+        val selectionArgs = arrayOf("%" + folderName + "%")
+
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val selection = MediaStore.Images.Media.DATA + " like ? "
+
+        val projectionOnlyBucket = arrayOf(MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME)
+
+        cursorBucket = activity.contentResolver.query(uri, projectionOnlyBucket, selection, selectionArgs, null)
+
+        column_index_data = cursorBucket.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
+
+        while (cursorBucket.moveToNext()) {
+            absolutePathOfImage = cursorBucket.getString(column_index_data)
+            if (absolutePathOfImage != "" && absolutePathOfImage != null)
+                listOfAllImages.add(absolutePathOfImage)
+        }
+        return listOfAllImages.asReversed()
+    }
+
+}
